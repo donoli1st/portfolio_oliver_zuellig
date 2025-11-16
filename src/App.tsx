9a30ed23dd2a012f02f3f06263a27d4f72d91e26
donoli1st <oliverzuellig@gmail.com>
2025-11-16 18:33:57 +0100

    Testings with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="container">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className='btn btn-primary' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
